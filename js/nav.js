f7a4a401beda509a2ffe2d391a403d23a94c780f
NIkolayrr <niki_r95@yahoo.com>
2017-02-24 16:09:56 +0200

    Convert image to inline-svg

--- js/nav.js
+++ js/nav.js
@@ -31,4 +31,38 @@
         }
     });
 
+    $('img.svg').each(function(){
+        let $img = $(this);
+        let imgID = $img.attr('id');
+        let imgClass = $img.attr('class');
+        let imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            let $svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+
 })();
